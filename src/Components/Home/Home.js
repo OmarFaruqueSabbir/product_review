e5354cf659c513d888fd9936810ad9b1fe4d2f27
OmarFaruqueSabbir <avrosabbir18@gmail.com>
2022-04-05 16:27:54 +0600

    Button Name changed & linked in Home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -16,7 +16,10 @@ const Home = () => {
                         <h1 className='text-6xl font-bold mb-3 text-orange-500'>Find Best Watche'S</h1>
                         <p className='text-l pr-10 text-left text-slate-700'>Welcome to Watch World. Here you can find our exclusive collections of watches. We have Original Branded Watches for Gents, Ladies, Kids with Analog, Digital and Smartwatch variation. We have popular brands like- Fastrack, Casio, Fossil, Titan and so on. <strong>“Watch World”</strong> is with you for a long time with quality products & Excellent customer services. Customer Satisfaction is our Prime Goal.</p>
 
-                        <button className='bg-indigo-500 p-3 border rounded-r-lg rounded-l-lg text-white text-xl mt-3'>Live Demo</button>
+                        <Link to='/about'>
+                            <button className='bg-gray-800 p-3 border rounded-r-lg rounded-l-lg text-white text-xl mt-3'>About US</button>
+                        </Link>
+
                     </div>
 
                     <div className='flex sm:justify-center'>
@@ -32,16 +35,16 @@ const Home = () => {
             <div className='grid grid-cols-1 md:grid-cols-3 gap-4 justify-items-center'>
                 {
                     reviews.slice(0, 3).map((review) => (
-                    <ReviewHome key={review.id} review={review}>
-                    </ReviewHome>)
+                        <ReviewHome key={review.id} review={review}>
+                        </ReviewHome>)
                     )
                 }
             </div>
 
             <Link to='/reviews'>
-            <div className='mt-5 mb-10'>
-                <button className='bg-black p-3 border rounded-r-lg rounded-l-lg text-white text-xl mt-3'>More Reviews</button>
-            </div>
+                <div className='mt-5 mb-10'>
+                    <button className='bg-black p-3 border rounded-r-lg rounded-l-lg text-white text-xl mt-3'>More Reviews</button>
+                </div>
             </Link>
         </section>
 
